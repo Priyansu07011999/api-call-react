e320ee09fac7258e15b8a2af1deba48a9bb6a5c0
Priyansu07011999 <147058839+Priyansu07011999@users.noreply.github.com>
2024-04-30 00:58:53 +0530

    Update MoviesList.jsx Storing and deleting the new movies into the Databases

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,21 +1,31 @@
+// Inside MoviesList.js
+
 import React from 'react';
 
-import Movie from './Movie';
-import classes from './MoviesList.module.css';
+const MoviesList = ({ movies, onDelete }) => {
+  const deleteHandler = (id) => {
+    onDelete(id);
+  };
 
-const MovieList = (props) => {
   return (
-    <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
-        />
+    <ul className="divide-y divide-gray-200">
+      {movies.map((movie) => (
+        <li key={movie.id} className="py-4 flex">
+          <div className="ml-3">
+            <p className="text-sm font-medium text-gray-900">{movie.title}</p>
+            <p className="text-sm text-gray-500">{movie.openingText}</p>
+            <p className="text-sm text-gray-500">{movie.releaseDate}</p>
+          </div>
+          <button
+            className="ml-auto bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => deleteHandler(movie.id)}
+          >
+            Delete
+          </button>
+        </li>
       ))}
     </ul>
   );
 };
 
-export default MovieList;
+export default MoviesList;
